ca2a9eba9993346993112545a120c74b1025a7c6
Julio Navarrete <icesasoftcorp@gmail.com>
2024-04-29 19:24:04 -0500

    Update Varibales.dart

--- 2.variables/ejercicios/julio_navarrete/variables.dart
+++ 2.variables/ejercicios/julio_navarrete/variables.dart
@@ -1,5 +1,17 @@
 void main(List<String> args) {
   var name = "Julio";
-  var lastName = "Navarrete";
+  print(name);
+  name = "Julio javier";
+  final canridad_carros = 5;
+  print(canridad_carros);
+  print(name);
+
+  const lastName = "navarrete";
+  print(lastName);
   var age = 35;
+  age = 34;
+  late String fullName;
+  fullName='29/04/2024';
+  print(fullName);
+  print(fullName);
 }
